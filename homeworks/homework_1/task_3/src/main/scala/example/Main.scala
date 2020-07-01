b2fa5a4ddeef5b887f7781be90da6aab1ba5dd59
Vaist <tjavaistt@gmail.com>
2020-07-01 11:01:26 +0300

    task 3

---
+++ homeworks/homework_1/task_3/src/main/scala/example/Main.scala
@@ -0,0 +1,11 @@
+package example
+
+object Main extends App {
+  var greeting = "Hello Scala! This is Vaist"
+
+  println(greeting)
+
+  println(greeting.replace("Hello", "Hola"))
+
+  println(greeting.reverse)
+}
