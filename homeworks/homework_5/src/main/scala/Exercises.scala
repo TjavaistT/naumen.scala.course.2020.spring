b939dd7a7553fc2975122e45290f156e9d7c0b9d
Vaist <tjavaistt@gmail.com>
2020-08-08 08:47:00 +0300

    Parametrized types

--- homeworks/homework_5/src/main/scala/Exercises.scala
+++ homeworks/homework_5/src/main/scala/Exercises.scala
@@ -7,17 +7,40 @@ object Exercises {
 
   case class Dog(override val name: String) extends Animal
 
+  case class Shelter[+Pet <: Animal](pets: List[Pet]){
 
+    def + [GenerType >: Pet <: Animal](pet: GenerType): Shelter[GenerType] = {
+      new Shelter[GenerType](pet :: pets)
+    }
 
-  case class Shelter ...
+    def ++ [GenerType >: Pet <: Animal](newPets: Shelter[GenerType]): Shelter[GenerType]= {
+      Shelter[GenerType](pets ++ newPets.pets)
+    }
 
+    def getNames: List[String] = {
+      pets.map(pet => pet.name).toList
+    }
 
+    def feed(food: Food[Pet]): List[String] = {
+      pets.map(pet =>food.feed(pet)).toList
+    }
+  }
 
-  trait Food ...
+  trait Food[-Pet <: Animal] {
+    def feed(pet: Pet): String = {
+      pet.name + " eats " + toString
+    }
+  }
 
-  case object Meat extends Food[Animal] ...
+  case object Meat extends Food[Animal]{
+    override def toString: String = "meat"
+  }
 
-  case object Milk extends Food[Cat] ...
+  case object Milk extends Food[Cat] {
+    override def toString: String = "milk"
+  }
 
-  case object Bread extends Food[Dog] ...
+  case object Bread extends Food[Dog] {
+    override def toString: String = "bread"
+  }
 }
